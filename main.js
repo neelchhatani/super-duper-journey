167579d40b7446977418c9e3af3eff85709467de
neelchhatani <92216090+neelchhatani@users.noreply.github.com>
2021-10-10 19:37:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload(){
+
+}
+
+function setup(){
+canvas = createCanvas(500,500);
+canvas.position(450,200);
+}
+
+function draw(){
+
+}
+
+function takesnapshot(){
+save('project.png');
+}
